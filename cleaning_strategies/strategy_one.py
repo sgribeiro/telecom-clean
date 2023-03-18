fc29b7e78d1bc75ef50cd219b99b1105bcaae8f0
Samuel G. Ribeiro <81725314+sgribeiro@users.noreply.github.com>
2023-03-18 12:45:06 -0300

    Update Project
    Atualização da subseção Tratamento de Valores Ausentes, da seção de Estratégias de Análise;
    Atualização do arquivo strategy_one.py, módulo de limpeza e tratamento de valores ausentes.

--- cleaning_strategies/strategy_one.py
+++ cleaning_strategies/strategy_one.py
@@ -12,10 +12,29 @@ def percent_missing(DataFrame):
         DataFrame (pandas.DataFrame): DataFrame de entrada a ser analisado.
 
     Returns:
-        str: String contendo a porcentagem de valores faltantes no DataFrame de entrada, arredondada para duas casas decimais,
+        str: String contendo a porcentagem de valores faltantes no DataFrame de entrada, com duas casas decimais,
         seguida pelo símbolo "%".
     """
+
     percent_missing = DataFrame.isnull().mean().mean() * 100
+    return f"{round(percent_missing, 2)}%"
+
+
+def lines_percent_missing(DataFrame):
+    """
+    Retorna uma string com a porcentagem de linhas com valores ausentes no DataFrame de entrada. 
+
+    Args:
+        DataFrame (pandas.DataFrame): DataFrame a ser analisado.
+
+    Returns:
+        str: String contendo a porcentagem de linhas com valores faltantes no DataFrame de entrada, com duas casas decimais,
+        seguida pelo símbolo "%".
+    """
+
+    # Calcula o número total de linhas com valores ausentes e divide pela quantidade de linhas
+    percent_missing = DataFrame.isnull().any(axis=1).mean() * 100
+
     return f"{round(percent_missing, 2)}%"
 
 def columns_percent_missing(DataFrame):
@@ -30,8 +49,8 @@ def columns_percent_missing(DataFrame):
         DataFrame (pandas.DataFrame): DataFrame com as colunas do DataFrame de entrada, mostrando a quantidade e porcentagem de valores faltantes 
         em cada coluna, bem como o tipo de dados de cada coluna. 
     """
-    columns_missing = pd.DataFrame({'Missing Values': DataFrame.isnull().sum(), 
+    missing_columns = pd.DataFrame({'Missing Values': DataFrame.isnull().sum(), 
                                     'Missing Values (%)': DataFrame.isnull().mean() * 100,
                                     'DType': DataFrame.dtypes})
 
-    return columns_missing.sort_values('Missing Values (%)', ascending=False).round(2)
+    return missing_columns.sort_values('Missing Values (%)', ascending = False).round(2)
